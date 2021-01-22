faaca919b1d2dfc28ce3f1222bc28ea3d7249097
Wojciech Dziwoki <wdziwoki@grupaeuro.pl>
2021-01-22 15:46:19 +0100

    ustawienia pod heroku

--- calc/calc/settings.py
+++ calc/calc/settings.py
@@ -143,3 +143,18 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
 ]
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
